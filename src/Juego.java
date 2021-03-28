e8fb7e4ba9255177eab98cfd202750f415edcdb3
cricripaz <cricripaz6@gmail.com>
2021-03-28 16:20:50 -0400

    terminado :V

--- src/Juego.java
+++ src/Juego.java
@@ -85,6 +85,9 @@ public class Juego {
             propiedades[35] = new Propiedad( false , "F. Short Line" , 200 , "Ferrocarril" ,false,true);
             propiedades[5] = new Propiedad( false , "F. Reading" , 200 , "Ferrocarril" ,false,true);
             propiedades[25] = new Propiedad( false , "F. B & O" , 200 , "Ferrocarril" ,false,true);
+            propiedades[2]= new Propiedad(false,"ArcaComunal",0,"ninguno",true,false);
+            propiedades[17]=new Propiedad(false,"ArcaComunal",0,"ninguno",true,false);
+            
 
 
 //           for (int i = 0; i <propiedades.length ; i++) {
@@ -92,23 +95,26 @@ public class Juego {
 //           }
     }
 
-    void crearTarjetasArc(){
+    void crearArcaComunal() {
 
 
-            arcaComunal[0] = new ArcaComunal( "Canal 13 Gana 15 Martin FIERRO" , "gana 50" , 50 );
-            arcaComunal[1] = new ArcaComunal( "Feriagro es todo un exito" , "gana 100" , 100 );
-            arcaComunal[2] = new ArcaComunal( "Hector Magneto debe operarse" , "pague 50" , -50 );
-            arcaComunal[3] = new ArcaComunal( "Editorial Ivrea de revistas sigue en alza" , "gana 75" , 75 );
-            arcaComunal[4] = new ArcaComunal( "Sus abogados lograron sacarlo libre de la carcel" , "Libre de la cacel" , 50 );
-            arcaComunal[5] = new ArcaComunal( "La justicia lo acusa de armar una revolucion vaya para la carcel" , "Va para la carcel" , -50);
-            arcaComunal[6] = new ArcaComunal( "Victor Hugo lo denuncia y lo hace perder apoyo" , "pague 20", -20 );
-            arcaComunal[7] = new ArcaComunal( "Quieren quitarle su parte de la empresa editorial" , "Debe pagar 100 para sus abogados" , -100 );
-            arcaComunal[8] = new ArcaComunal( "Baja la soja" , "pierde 25" , 25 );
-            arcaComunal[9] = new ArcaComunal( "Usted gano el concurso de belleza gana 50" , "gana 50" , 50 );
-            arcaComunal[10] = new ArcaComunal( "Su editorial gana un premio a la mejor editorial" , "gana 25" , 50 );
-            arcaComunal[11] = new ArcaComunal( "Con recursos de amparo se logran frenar la ley de medios" , "pierde 50" , -50 );
-            arcaComunal[12] = new ArcaComunal( "Rechazan la fusion cablevision multicanal" , "pierde 25", -25 );
+        arcaComunal[0] = new ArcaComunal("Canal 13 Gana 15 Martin FIERRO", "gana 50", 50);
+        arcaComunal[1] = new ArcaComunal("Feriagro es todo un exito", "gana 100", 100);
+        arcaComunal[2] = new ArcaComunal("Hector Magneto debe operarse", "pague 50", -50);
+        arcaComunal[3] = new ArcaComunal("Editorial Ivrea de revistas sigue en alza", "gana 75", 75);
+        arcaComunal[4] = new ArcaComunal("Sus abogados lograron sacarlo libre de la carcel", "Libre de la cacel", 50);
+        arcaComunal[5] = new ArcaComunal("La justicia lo acusa de armar una revolucion vaya para la carcel", "Va para la carcel", -50);
+        arcaComunal[6] = new ArcaComunal("Victor Hugo lo denuncia y lo hace perder apoyo", "pague 20", -20);
+        arcaComunal[7] = new ArcaComunal("Quieren quitarle su parte de la empresa editorial", "Debe pagar 100 para sus abogados", -100);
+        arcaComunal[8] = new ArcaComunal("Baja la soja", "pierde 25", 25);
+        arcaComunal[9] = new ArcaComunal("Usted gano el concurso de belleza gana 50", "gana 50", 50);
+        arcaComunal[10] = new ArcaComunal("Su editorial gana un premio a la mejor editorial", "gana 25", 50);
+        arcaComunal[11] = new ArcaComunal("Con recursos de amparo se logran frenar la ley de medios", "pierde 50", -50);
+        arcaComunal[12] = new ArcaComunal("Rechazan la fusion cablevision multicanal", "pierde 25", -25);
 
+    }
+
+    void crearCasualidad(){
             casualidades[0]= new Casualidad( "Las editoriales de libros escolares pega una licitacion" ,"gana 50" ,50);
             casualidades[1]= new Casualidad( "La AFIP realiza un operativo en clarin" , "pierde 25" , -25 );
             casualidades[2]= new Casualidad( "Logra meter un diputado en el congreso" , "gana 100" , 100 );
@@ -150,15 +156,14 @@ public class Juego {
             bienvenida(jugadores);
 
             crearPropiedadesTablero();
-            int jugador = 0;
-            for (int i = 0; i < 11 ; i++) {
 
-
+            for (int i = 0; i < 10 ; i++) {
+                System.out.println(" Ronda Numero  : " + i  );
                 turnoJugador();
 
-
-
             }
+            
+            verificarGanador();
         }else if (cantidadJugadores==3){
 
             jugadores = new Jugador[3];
@@ -175,6 +180,11 @@ public class Juego {
 
             bienvenida(jugadores);
 
+            crearPropiedadesTablero();
+            for (int i = 0; i < 11 ; i++) {
+                turnoJugador();
+
+            }
         }else if (cantidadJugadores ==4){
 
             jugadores = new Jugador[4];
@@ -190,17 +200,29 @@ public class Juego {
                 jugadores[i].setNombre(nombre);
             }
             bienvenida(jugadores);
+
+            crearPropiedadesTablero();
+            for (int i = 0; i < 11 ; i++) {
+                turnoJugador();
+
+            }
+
+
+
         }
 
 
     }
 
-
+  
 
     void bienvenida(Jugador[]jugadores ){
+
+        System.out.println("------------------------------------");
         for (int i = 0; i < jugadores.length ; i++) {
             System.out.println("Bienvenido : " + jugadores[i].getNombre() );
         }
+        System.out.println("------------------------------------");
     }
 
 
@@ -209,17 +231,28 @@ public class Juego {
         int dado;
 
         for (int i = 0; i < jugadores.length ; i++) {
-           System.out.println("turno del : "+ jugadores[i].getNombre());
+            System.out.println("------------------------------------");
+           System.out.println("turno de : "+ jugadores[i].getNombre());
             dado = ((int)(Math.random()*6))+1 + ((int)(Math.random()*6)) + 1 ;
            System.out.println("Dados = " + dado );
             jugadores[i].setPosicion(jugadores[i].getPosicion()+dado);
+            verificarNoSalgaTablero(jugadores[i].getPosicion(),i);
+            paradaLibre(jugadores[i].getPosicion());
+            verificarCarcel(jugadores[i].getPosicion(),i);
             System.out.println("posicion " + jugadores[i].getNombre() + " = " +jugadores[i].getPosicion());
+            System.out.println(" Dinero Del Jugador : " + jugadores[i].getDinero());
 
-            verificarSiEstaEnPropiedad(i,dado);
+            verificarSiEstaEnPropiedad(i);
+
+
+            System.out.println("------------------------------------");
         }
     }
 
-    void verificarSiEstaEnPropiedad(int numeroJugador,int posicionPropiedad){
+    void verificarSiEstaEnPropiedad(int numeroJugador){
+
+                int posicionPropiedad = jugadores[numeroJugador].getPosicion();
+
 
             if(propiedades[posicionPropiedad].isPropiedad()){
                 if(!propiedades[posicionPropiedad].isTieneDueno()){
@@ -229,11 +262,29 @@ public class Juego {
                 }
 
             }else{
+
                 if (propiedades[posicionPropiedad].getNombre() == "GO" ){
+                        System.out.println("Estas En GO Recibe 200");
+                        jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() + 200);
 
-                }else if(propiedades[posicionPropiedad].getNombre() == "ArcaComunal" ){
+                }else if(propiedades[posicionPropiedad].getNombre() == "ArcaComunal"){
+                         crearArcaComunal();
+                        System.out.println("ohh ARCA COMUNAL obtienes 1 tarjeta !!! ");
+                         int sorteo = ((int)(Math.random()*arcaComunal.length)) -1;
+                      System.out.println("Texto :" + arcaComunal[sorteo].getTexto() );
+                      System.out.println(" Accion " + arcaComunal[sorteo].getAccion());
 
-                }else if(propiedades[posicionPropiedad].getNombre() == "Casualida" ){
+                      jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() + (arcaComunal[sorteo].getValor()));
+
+                }else if(propiedades[posicionPropiedad].getNombre() == "Casualidad" ){
+
+                    System.out.println("ohh Casualidad obtienes 1 tarjeta !!! ");
+                    int sorteo = ((int)(Math.random()*casualidades.length))-1;
+                    System.out.println("Texto :" + casualidades[sorteo].getTexto() );
+                    System.out.println(" Accion " + casualidades[sorteo].getAccion());
+
+                    jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() + (casualidades[sorteo].getValor()));
+
 
                 }
             }
@@ -243,22 +294,74 @@ public class Juego {
 
      void menuCompraPropiedad(int posicionPropiedad,int numeroJugador) {
         int opcion ;
+        System.out.println("----------------------------------------------------------");
         System.out.println("Usted ha Caido en Una propiedad ");
-        System.out.println("Desea Comprar la Proiedad "+ propiedades[posicionPropiedad].getNombre());
+        System.out.println("Desea Comprar la Propiedad "+ propiedades[posicionPropiedad].getNombre());
+        System.out.println("PRECIO : " + propiedades[posicionPropiedad].getValor());
         System.out.println("1) Si ");
         System.out.println("2) NO ");
         System.out.println("Introduzca una opcion");
         opcion=teclado.nextInt();
         if (opcion == 1){
-            propiedades[posicionPropiedad].setTieneDueno(true);
-            jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() - propiedades[posicionPropiedad].getValor());
-            System.out.println("Cantidad De Dinero Del Jugador : "+ jugadores[numeroJugador].getNombre() + " Dinero " + jugadores[numeroJugador].getDinero());
-            jugadores[numeroJugador].setCantidadPropiedades(jugadores[numeroJugador].getCantidadPropiedades() + 1);
-            System.out.println("Cantidad De Propiedades Del Jugadros : " +jugadores[numeroJugador].getNombre() + "Propiedades : " + jugadores[numeroJugador].getCantidadPropiedades());
+            if(propiedades[posicionPropiedad].getValor()>jugadores[numeroJugador].getDinero()) {
+
+                    System.out.println("NO PUEDES COMPRAR LA PROPIEDAD TE FALTA DINERO");
+                    System.out.println(" Dinero = "+ jugadores[numeroJugador].getDinero());
+
+            }else{
+                propiedades[posicionPropiedad].setTieneDueno(true);
+                jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() - propiedades[posicionPropiedad].getValor());
+                System.out.println("Cantidad De Dinero Del Jugador : " + jugadores[numeroJugador].getNombre() + " Dinero " + jugadores[numeroJugador].getDinero());
+                jugadores[numeroJugador].setCantidadPropiedades(jugadores[numeroJugador].getCantidadPropiedades() + 1);
+                System.out.println("Cantidad De Propiedades Del Jugadros : " + jugadores[numeroJugador].getNombre() + "Propiedades : " + jugadores[numeroJugador].getCantidadPropiedades());
+            }
         }else {
             System.out.println("continuando el Juego");
         }
+
+         System.out.println("----------------------------------------------------------");
     }
+
+
+    void verificarNoSalgaTablero(int posicion ,int numeroJugador) {
+        if (posicion > 39 ) {
+            jugadores[numeroJugador].setPosicion(0);
+        }
+    }
+
+    void paradaLibre(int posicion){
+        if (posicion == 20 ){
+            System.out.println("OHH Estas En Parada Libre");
+        }else if(posicion == 10){
+            System.out.println("ohh pasaste por la carcel ");
+        }
+    }
+    void verificarCarcel (int posicion , int numeroJugador){
+        if (posicion == 30){
+            System.out.println("_______________________________________");
+            System.out.println("VAYA A LA CARCEL");
+            System.out.println("Se Le Cobrara 200");
+            jugadores[numeroJugador].setPosicion(10);
+            jugadores[numeroJugador].setDinero(jugadores[numeroJugador].getDinero() -200);
+            System.out.println("_______________________________________");
+
+        }
+    }
+
+    void verificarGanador() {
+
+        int ganador = jugadores[0].getCantidadPropiedades();
+        int posicionGanador = 0;
+        for (int i = 0; i < jugadores.length ; i++) {
+            if(jugadores[i].getCantidadPropiedades()>ganador){
+                ganador = jugadores[i].getDinero();
+                posicionGanador = i ;
+            }
+        }
+
+        System.out.println("Ganador es : " + jugadores[posicionGanador].getNombre() + " Felicidades !!!!!") ;
+    }
+
 
 
 }
